ce8677c2e97a09d03e7a15bf4095edec1dab85fd
Alek Cvetkovski <A.Cvetkovski.inf21@stud.bbbaden.ch>
2023-10-27 15:25:31 +0200

    updated app.py & test_app

--- test_app.py
+++ test_app.py
@@ -7,22 +7,24 @@ import pytest
 
 @pytest.fixture()
 def client():
-    app.config['TESTING'] = True
+    app.config["TESTING"] = True
     client = app.test_client()
 
     yield client
 
 
-def test_add_entry(client):
-    # Test adding an entry
-    response = client.post(
-        '/add_entry', data={'content': 'Test Entry Content'})
+def test_add_entry_with_happiness(client):
+    # Simulate entries list (for testing purposes)
+    entries = []
+
+    response = client.post("/add_entry", data={"content": "Test Entry Content"})
 
     # Check if the response is a redirect to the index page
     assert response.status_code == 302
-    assert response.headers['Location'] == '/'
+    assert response.headers["Location"] == "/"
 
-    # Check if the entry was added to the database
+    # Check if the entry was added to the temporary list
     entry = entries[0]
     assert entry is not None
-    assert entry.content == 'Test Entry Content'
+    assert entry["content"] == "Test Entry Content"
+    assert entry["happiness"] == "😃"
